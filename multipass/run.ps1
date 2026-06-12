e4d247a2a439f600cb856635d669badbb90a9602
Christopher Schuf <christopherschuf@MBPvonCistopher.fritz.box>
2026-06-12 12:19:00 +0200

    Remove controller-2 from Multipass setup

--- multipass/run.ps1
+++ multipass/run.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = "Stop"
 
 $VMs = @(
     "controller-1",
-    "controller-2",
     "loadbalancer",
     "worker-1",
     "worker-2"
@@ -10,25 +9,21 @@ $VMs = @(
 
 Write-Host "Starting VMs"
 
-multipass launch --cloud-init controller-cloud-config.yml --disk 5G --memory 1G --cpus 1 --name controller-1
+multipass launch --cloud-init controller-cloud-config.yml --disk 10G --memory 1G --cpus 1 --name controller-1
 Start-Sleep -Seconds 20
 
-multipass launch --cloud-init controller-cloud-config.yml --disk 5G --memory 1G --cpus 1 --name controller-2
+multipass launch --cloud-init controller-cloud-config.yml --disk 10G --memory 1G --cpus 1 --name loadbalancer
 Start-Sleep -Seconds 20
 
-multipass launch --cloud-init controller-cloud-config.yml --disk 5G --memory 1G --cpus 1 --name loadbalancer
+multipass launch --cloud-init worker-cloud-config.yml --disk 15G --memory 2G --cpus 1 --name worker-1
 Start-Sleep -Seconds 20
 
-multipass launch --cloud-init worker-cloud-config.yml --disk 5G --memory 5G --cpus 1 --name worker-1
-Start-Sleep -Seconds 20
-
-multipass launch --cloud-init worker-cloud-config.yml --disk 5G --memory 5G --cpus 1 --name worker-2
+multipass launch --cloud-init worker-cloud-config.yml --disk 15G --memory 2G --cpus 1 --name worker-2
 Start-Sleep -Seconds 30
 
 Write-Host "Copying netplan yaml"
 
 multipass transfer 01-controller-1-network.yaml controller-1`:01-controller-1-network.yaml
-multipass transfer 01-controller-2-network.yaml controller-2`:01-controller-2-network.yaml
 multipass transfer 01-loadbalancer-network.yaml loadbalancer`:01-loadbalancer-network.yaml
 multipass transfer 01-worker-1-network.yaml worker-1`:01-worker-1-network.yaml
 multipass transfer 01-worker-2-network.yaml worker-2`:01-worker-2-network.yaml
@@ -36,7 +31,6 @@ multipass transfer 01-worker-2-network.yaml worker-2`:01-worker-2-network.yaml
 Write-Host "Installing netplan configs"
 
 multipass exec controller-1 -- sudo cp 01-controller-1-network.yaml /etc/netplan/99-kubernetes.yaml
-multipass exec controller-2 -- sudo cp 01-controller-2-network.yaml /etc/netplan/99-kubernetes.yaml
 multipass exec loadbalancer -- sudo cp 01-loadbalancer-network.yaml /etc/netplan/99-kubernetes.yaml
 multipass exec worker-1 -- sudo cp 01-worker-1-network.yaml /etc/netplan/99-kubernetes.yaml
 multipass exec worker-2 -- sudo cp 01-worker-2-network.yaml /etc/netplan/99-kubernetes.yaml
